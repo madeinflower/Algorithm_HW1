c9d00fac2d32dadba69c096f97677b8d07392d03
madeinflower <tatsenko.tetiana@gmail.com>
2024-06-17 14:12:01 +0200

    code.pesudocode.scheme

---
+++ src/main/java/code/Main.java
@@ -0,0 +1,24 @@
+package code;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        SumThreeNumbers calculator = new SumThreeNumbers();
+
+        calculator.sumAndPrint(num1, num2, num3);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/code/SumThreeNumbers.java
@@ -0,0 +1,8 @@
+package code;
+
+public class SumThreeNumbers {
+    public void sumAndPrint(int num1, int num2, int num3) {
+        int sum = num1 + num2 + num3;
+        System.out.println("Total: " + sum);
+    }
+}
